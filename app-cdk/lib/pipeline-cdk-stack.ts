ac06deb5fff10160c76209874af7d23ecd67ecfc
Lee C <lee.croucher@naimuri.com>
2025-06-17 11:39:07 +0000

    Added Docker build and push

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -1,12 +1,18 @@
-import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { aws_codeconnections as codeconnections } from 'aws-cdk-lib';
 import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
 import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
+interface ConsumerProps extends StackProps {
+  ecrRepository: ecr.Repository;
+}
 
 export class PipelineCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props: StackProps) {
+  constructor(scope: Construct, id: string, props: ConsumerProps) {
     super(scope, id, props);
 
     const SourceConnection = new codeconnections.CfnConnection(this, 'CICD_Workshop_Connection', {
@@ -21,7 +27,10 @@ export class PipelineCdkStack extends Stack {
       executionMode: codepipeline.ExecutionMode.QUEUED,
     });
 
-    const codeBuild = new codebuild.PipelineProject( this, 'CodeBuild', {
+    const codeBuild = new codebuild.PipelineProject(
+      this,
+      'CodeBuild',
+      {
         environment: {
           buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
           privileged: true,
@@ -56,6 +65,55 @@ export class PipelineCdkStack extends Stack {
           project: codeBuild,
           input: sourceOutput,
           outputs: [unitTestOutput],
+        }),
+      ],
+    });
+
+    const dockerBuild = new codebuild.PipelineProject(this, 'DockerBuild', {
+      environmentVariables: {
+        IMAGE_TAG: { value: 'latest' },
+        IMAGE_REPO_URI: { value: props.ecrRepository.repositoryUri },
+        AWS_DEFAULT_REGION: { value: process.env.CDK_DEFAULT_REGION },
+      },
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
+        privileged: true,
+        computeType: codebuild.ComputeType.LARGE,
+      },
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_docker.yml'),
+    });
+
+    const dockerBuildRolePolicy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ['*'],
+      actions: [
+        'ecr:GetAuthorizationToken',
+        'ecr:BatchCheckLayerAvailability',
+        'ecr:GetDownloadUrlForLayer',
+        'ecr:GetRepositoryPolicy',
+        'ecr:DescribeRepositories',
+        'ecr:ListImages',
+        'ecr:DescribeImages',
+        'ecr:BatchGetImage',
+        'ecr:InitiateLayerUpload',
+        'ecr:UploadLayerPart',
+        'ecr:CompleteLayerUpload',
+        'ecr:PutImage',
+      ],
+    });
+
+    dockerBuild.addToRolePolicy(dockerBuildRolePolicy);
+
+    const dockerBuildOutput = new codepipeline.Artifact();
+
+    pipeline.addStage({
+      stageName: 'Docker-Push-ECR',
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: 'Docker-Build',
+          project: dockerBuild,
+          input: sourceOutput,
+          outputs: [dockerBuildOutput],
         }),
       ],
     });
